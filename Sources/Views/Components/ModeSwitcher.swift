b93c855a9bca19046104c6ceec1c73dad86de84f
m78srs6sys-design <m78srs6sys@privaterelay.appleid.com>
2026-08-18 14:31:54 +0800

    fix Sources/Views/Components/ModeSwitcher.swift

--- Sources/Views/Components/ModeSwitcher.swift
+++ Sources/Views/Components/ModeSwitcher.swift
@@ -29,7 +29,7 @@ struct ModeSwitcher: View {
                             }
                         )
                 }
-                .buttonStyle(BounceButtonStyle)
+                .buttonStyle(BounceButtonStyle())
             }
         }
         .padding(4)
